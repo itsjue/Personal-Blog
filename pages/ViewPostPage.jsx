4f1d104ba8d962f0d365e0f7f29fda0db53d52ea
Jue Parinthon <jue.prt@hotmail.com>
2025-09-12 17:38:51 +0700

    feat: add ResetPasswordCard component; integrate into ViewPostPage; update SignupSuccessfully to use Link for navigation

--- pages/ViewPostPage.jsx
+++ pages/ViewPostPage.jsx
@@ -1,10 +1,12 @@
 import { Footer } from "@/components/Nav-Hero-FooterSection";
 import ViewPostSection from "@/components/ViewPostSection";
+import ResetPasswordCard from "@/components/cards/ResetPasswordCard";
 
 function ViewPostPage() {
   return (
     <>
     <ViewPostSection />
+        <ResetPasswordCard />
     <Footer />
     </>
   )

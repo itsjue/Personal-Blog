c458bcd84cf3c7f4d48c3ecdab3b45568141f02d
Jue Parinthon <jue.prt@hotmail.com>
2025-09-17 04:19:58 +0700

    feat: update AdminArticleSection layout and add icons for navigation buttons

--- src/components/AdminArticleSection.jsx
+++ src/components/AdminArticleSection.jsx
@@ -8,8 +8,8 @@ function AdminArticleSection() {
   return (
     <>
       <div>
-        <div className="flex w-full border">
-          <div className="w-[280px] h-screen bg-[#EFEEEB]">
+        <div className="flex">
+          <div className="flex flex-col w-[280px] h-screen bg-[#EFEEEB]">
             <div className="pt-[74.15px] pl-[26.2px]">
               <img
                 src="/src/assets/logo.png"
@@ -20,31 +20,83 @@ function AdminArticleSection() {
                 Admin Panel
               </h4>
             </div>
-            <div>
-                <div>
-                <button>Article management</button>
-                <button>Category management</button>
-                <button>Profile</button>
-                <button>Notofication</button>
-                <button>Reset password</button>
+            <div className="mt-[60px]">
+              <div>
+                <div className="flex gap-3 py-5 px-6">
+                  <img
+                    src="/src/assets/notebook_icon.png"
+                    alt="notebook_icon"
+                    className="size-6"
+                  />
+                  <button>Article management</button>
                 </div>
+                <div className="flex gap-3 py-5 px-6">
+                  <img
+                    src="/src/assets/file_icon.png"
+                    alt="file_icon"
+                    className="size-6"
+                  />
+                  <button>Category management</button>
+                </div>
+                <div className="flex gap-3 py-5 px-6">
+                  <img
+                    src="/src/assets/user_icon.png"
+                    alt="user_icon"
+                    className="size-6"
+                  />
+                  <button>Profile</button>
+                </div>
+                <div className="flex gap-3 py-5 px-6">
+                  <img
+                    src="/src/assets/bell_icon.png"
+                    alt="bell_icon"
+                    className="size-6"
+                  />
+                  <button>Notification</button>
+                </div>
+                <div className="flex gap-3 py-5 px-6">
+                  <img
+                    src="/src/assets/reset_icon.png"
+                    alt="reset_icon"
+                    className="size-6"
+                  />
+                  <button>Reset password</button>
+                </div>
+              </div>
+              <div className="flex flex-col">
                 <button>hh. website</button>
-                <div>
-                    Log out
-                </div>
+                <button>Log out</button>
+              </div>
             </div>
           </div>
 
-          <div className="flex">
-            <div className="flex flex-col">
-              <div className="flex justify-around items-center h-[96px] w-screen border">
-                <h3 className="font-semibold text-6 text-[#26231E]">Article management</h3>
-                <button className="bg-[#26231E] text-white py-3 px-10 rounded-full">Create article</button>
+          <div className="flex flex-1">
+            <div className="flex flex-col flex-1">
+              <div className="flex justify-between items-center h-[96px] px-[60px] border-b border-[#DAD6D1]">
+                <h3 className="font-semibold text-6 text-[#26231E]">
+                  Article management
+                </h3>
+                <button className="bg-[#26231E] text-white py-3 px-10 rounded-full">
+                  Create article
+                </button>
               </div>
-              <div>
-                <input type="text" placeholder="Search..." />
-                <StatusDropdownMenuCheckboxes />
-                <CategoriesDropdownMenuCheckboxes />
+              <div className="flex items-center justify-between px-[60px]">
+                <div className="relative">
+                  <img
+                    src="/src/assets/search_icon.png"
+                    alt="search_icon"
+                    className="absolute size-6 translate-x-1/2 translate-y-1/2"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    className="max-w-[360px] font-medium py-3 px-11 bg-white border border-[#DAD6D1] rounded-xl placeholder:text-[#75716B]"
+                  />
+                </div>
+                <div>
+                  <StatusDropdownMenuCheckboxes />
+                  <CategoriesDropdownMenuCheckboxes />
+                </div>
               </div>
             </div>
           </div>
